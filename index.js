8425fcf0b208498a0e78c8b6194231cb2bcd6a52
Rubel Hossain99 <rubel820746@gmail.com>
2022-11-24 11:27:14 +0600

    Mongo db Config is Done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.nqqpx5x.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.nqqpx5x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
