a55e39314f13a8b70190558db238f67101731997
Rubel Hossain99 <rubel820746@gmail.com>
2022-11-24 11:21:41 +0600

    Add the Mongodb DataBase on the Server Site

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middle Ware
 app.use(cors());
 app.use(express.json());
+
+// MongoDB
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.nqqpx5x.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
