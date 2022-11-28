5149112e6def977a63ae2644610b0c8c34012cfc
Rubel Hossain99 <rubel820746@gmail.com>
2022-11-28 21:57:34 +0600

    Some Error Fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken")
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -13,6 +14,26 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.nqqpx5x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+/// jwt Verification Process
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('Unauthorizes access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 async function run() {
     try {
         const addBooksCollection = client.db('assignmenttwelve').collection('books');
@@ -20,42 +41,43 @@ async function run() {
         const regisCollection = client.db('assignmenttwelve').collection('regisusers');
 
         // ========================= AddBooksCollection Area =========================
-
-        
         app.post('/books', async (req, res) => {
             const books = req.body;
             const result = await addBooksCollection.insertOne(books);
             res.send(result);
         });
-        
 
-        app.get('/books', async (req, res) => {
-            let query = {};
-            if (req.query.email) {
-                query = {
-                    email: req.query.email
-                }
+        // This is a My booking Api Data with JWT
+        app.get('/books', verifyJWT, async (req, res) => {
+            const email = req.query.email;
+
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
             }
+
+            const query = { email: email }
             const cursor = addBooksCollection.find(query);
             const booksdata = await cursor.toArray();
             res.send(booksdata);
         });
-        
+
+
         app.get('/books/data/:id', async (req, res) => {
             const id = req.params.id;
             const singquery = { _id: ObjectId(id) };
             const singresult = await addBooksCollection.findOne(singquery);
             res.send(singresult);
         })
-        
+
         app.delete('/books/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await addBooksCollection.deleteOne(query);
             res.send(result);
         })
-        
-        
+
+
         app.put('/books/adsrun/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
@@ -154,7 +176,7 @@ async function run() {
             res.send(booksdata);
         });
 
-        /// User info verified
+        /// User verified add on Database
         app.put('/regisusers/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
@@ -168,6 +190,66 @@ async function run() {
             res.send(result);
         })
 
+        /// User verified add on Database
+        app.put('/regisusers/admin/:id', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail }
+            const user = await regisCollection.findOne(query);
+            if (user?.admin !== 'admin') {
+                return res.status(403).send({ message: 'forbidden Access' })
+            }
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    admin: "admin"
+                }
+            }
+            const result = await regisCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
+
+
+        /// Admin Checking
+        app.get('/regisusers/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await regisCollection.findOne(query);
+            res.send({ isAdmin: user?.admin === 'admin' });
+        })
+
+        /// Seller Checking
+        app.get('/regisusers/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await regisCollection.findOne(query);
+            res.send({ isSeller: user?.seller === 'seller' });
+        })
+
+
+
+        app.get('/regisusers/seller/', async (req, res) => {
+            const query = { role: 'seller' }
+            const cursor = regisCollection.find(query);
+            const allseller = await cursor.toArray();
+            res.send(allseller);
+        });
+
+        app.get('/regisusers/seller/', async (req, res) => {
+            const query = { role: 'seller' }
+            const cursor = regisCollection.find(query);
+            const allseller = await cursor.toArray();
+            res.send(allseller);
+        });
+        app.get('/regisusers/customer/', verifyJWT, async (req, res) => {
+            const query = { role: 'customer' }
+            const cursor = regisCollection.find(query);
+            const allseller = await cursor.toArray();
+            res.send(allseller);
+        });
+
         app.delete('/regisusers/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -176,17 +258,17 @@ async function run() {
         })
 
 
-        // // Json Web Toke (JWT)
-        // app.get('/jwt', async (req, res) => {
-        //     const email = req.query.email;
-        //     const query = { email: email };
-        //     const user = await addPostCollection.findOne(query);
-        //     if (user) {
-        //         const token = jwt.sign({ email }, process.env.JWT_TOKEN, { expiresIn: '1h' })
-        //         return res.send({ accessToken: token });
-        //     }
-        //     res.status(403).send({ accessToken: '' });
-        // });
+        // Json Web Toke (JWT)
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await regisCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '12h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' });
+        });
 
     }
     finally { }
